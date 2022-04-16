7431f85147a4cf32dcf7f8d39a2efb58fb1b8f73
badsk11 <103528714+badsk11@users.noreply.github.com>
2022-04-16 13:12:55 +0100

    Create script.sh

---
+++ script.sh
@@ -0,0 +1,9 @@
+echo Starting...
+curl -sSL https://get.docker.com/ | CHANNEL=stable bash
+systemctl enable --now docker
+GRUB_CMDLINE_LINUX_DEFAULT="swapaccount=1"
+mkdir -p /etc/pterodactyl
+curl -L -o /usr/local/bin/wings "https://github.com/pterodactyl/wings/releases/latest/download/wings_linux_$([[ "$(uname -m)" == "x86_64" ]] && echo "amd64" || echo "arm64")"
+chmod u+x /usr/local/bin/wings
+apt install certbot
+echo Finished
